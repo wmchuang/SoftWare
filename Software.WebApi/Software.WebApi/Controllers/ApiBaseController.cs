504707f42db025f6604427cb230a44c079419faf
CRole <515497782@qq.com>
2019-02-13 18:07:11 +0800

    init

---
+++ Software.WebApi/Software.WebApi/Controllers/ApiBaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Software.WebApi.Controllers
+{
+    /// <summary>
+    /// 接口基类
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    [Authorize]
+    public class ApiBaseController : ControllerBase
+    {
+
+    }
+}
---
+++ Software.WebApi/Software.WebApi/Startup.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Software.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        private const string ApiName = "Software";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region Swagger
+            var basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v0.1.0",
+                    Title = $"{ApiName} API",
+                    Description = "框架说明文档",
+                    TermsOfService = "None",
+                    Contact = new Swashbuckle.AspNetCore.Swagger.Contact { Name = ApiName, Email = "xxx@xxx.com", Url = "" }
+                });
+
+
+                //var xmlPath = Path.Combine(basePath, "Blog.Core.xml");//这个就是刚刚配置的xml文件名
+                //c.IncludeXmlComments(xmlPath, true);//默认的第二个参数是false，这个是controller的注释，记得修改
+
+                //var xmlModelPath = Path.Combine(basePath, "Blog.Core.Model.xml");//这个就是Model层的xml文件名
+                //c.IncludeXmlComments(xmlModelPath);
+
+                #region Token绑定到ConfigureServices
+
+                // 发行人
+                var IssuerName = (Configuration.GetSection("Audience"))["Issuer"];
+                var security = new Dictionary<string, IEnumerable<string>> { { IssuerName, new string[] { } }, };
+                c.AddSecurityRequirement(security);
+
+                //方案名称“Blog.Core”可自定义，上下一致即可
+                c.AddSecurityDefinition(IssuerName, new ApiKeyScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入Bearer {token}（注意两者之间是一个空格）\"",
+                    Name = "Authorization",//jwt默认的参数名称
+                    In = "header",//jwt默认存放Authorization信息的位置(请求头中)
+                    Type = "apiKey"
+                });
+                #endregion
+            });
+            #endregion
+
+            #region JWT Token Service
+            //读取配置文件
+            var audienceConfig = Configuration.GetSection("Audience");
+            var symmetricKeyAsBase64 = audienceConfig["Secret"];
+            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
+            var signingKey = new SymmetricSecurityKey(keyByteArray);
+
+            // 令牌验证参数
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = audienceConfig["Issuer"],//发行人
+                ValidateAudience = true,
+                ValidAudience = audienceConfig["Audience"],//订阅人
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                RequireExpirationTime = true,
+
+            };
+
+            //var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            //var permission = new List<Permission>();
+            //// 角色与接口的权限要求参数
+            //var permissionRequirement = new PermissionRequirement(
+            //    "/api/denied",// 拒绝授权的跳转地址（目前无用）
+            //    permission,
+            //    ClaimTypes.Role,//基于角色的授权
+            //    audienceConfig["Issuer"],//发行人
+            //    audienceConfig["Audience"],//听众
+            //    signingCredentials,//签名凭据
+            //    expiration: TimeSpan.FromDays(7)//接口的过期时间
+            //    );
+            //services.AddSingleton(permissionRequirement);
+
+            services.AddAuthentication(x =>
+               {
+                   x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                   x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+               }).AddJwtBearer(o =>
+               {
+                   o.TokenValidationParameters = tokenValidationParameters;
+               });
+            #endregion
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            // 使用静态文件
+            app.UseStaticFiles();
+
+            #region Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiHelp V1");
+                c.RoutePrefix = "";//路径配置，设置为空，表示直接在根域名（localhost:8001）访问该文件,注意localhost:8001/swagger是访问不到的，去launchSettings.json把launchUrl去掉
+                //js注入
+                c.InjectJavascript("/jquery/jquery.js");//jquery 插件
+                c.InjectJavascript("/swagger-lang.js");//我们自定义的js
+            });
+            #endregion
+
+            #region Authen
+            // 如果你想使用官方认证，必须在上边ConfigureService 中，配置JWT的认证服务
+            // .AddAuthentication 和 .AddJwtBearer 二者缺一不可
+            app.UseAuthentication();
+            #endregion
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
